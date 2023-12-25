8c627102a34e9a82bfa5121674565f5100be7cfa
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-25 18:48:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -16,22 +16,14 @@ int main(void) {
     cout << '<';
     while(!q.empty()) {
         for (int i = 0; i < k - 1; i++) {
-            int tmp = q.front();
+            q.push(q.front());
             q.pop();
-            if (q.empty()) {
-                cout << tmp << '>';
-                return 0;
-            }
-            q.push(tmp);
         }
-        if (q.size() == 1) {
-            cout << q.front();
-            q.pop();
-            break;
-        }
-        
-        cout << q.front() << ", ";
+
+        cout << q.front();
         q.pop();
+        if (q.size())
+            cout << ", ";
     }
     cout << '>';
 }
