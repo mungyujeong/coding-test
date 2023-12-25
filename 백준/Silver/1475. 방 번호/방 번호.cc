d5bef169ed90930297e19ad23fc8db3c1935b609
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-25 11:02:08 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -1,33 +1,30 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 int freq[10];
-int plastic[10];
 string str;
 int answer;
 
 int main(void) {
-    fill(freq, freq + 10, 1);
     
     cin >> str;
     for (auto i : str) {
-        if (plastic[i - '0']) {
-            plastic[i - '0']--;
-        } else if ((i == '6' || i == '9') && 
-                   !(plastic[6] == 0 && plastic[9] == 0)) {
-            if (plastic[6] == 0)
-                plastic[9]--;
-            else if (plastic[9] == 0)
-                plastic[6]--;
+        int idx = i - '0';
+        
+        if (idx == 9 || idx == 6) {
+            if (freq[6] > freq[9])
+                freq[9]++;
+            else
+                freq[6]++;
         } else {
-            answer++;
-            for (int j = 0; j < 10; j++)
-                plastic[j] += freq[j];
-            plastic[i - '0']--;
+            freq[idx]++;
         }
     }
+    
+    answer = *max_element(freq, freq + 10);
     
     cout << answer;
 }
