c7a72a6d2ef5633ab70d4624e84e9c224ed0a528
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-26 23:13:26 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2488 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -16,23 +16,9 @@ int main() {
 
         stack<char> stk;
         for (auto s : str) {
-            if (stk.empty()) {
-                stk.push(s);
-                continue;
-            } 
-
-            if (s == 'A') {
-                if (stk.top() == 'A') {
-                    stk.pop();
-                } 
-                else stk.push(s);
-            }
-            else if (s == 'B') {
-                if (stk.top() == 'B') {
-                    stk.pop();
-                }
-                else stk.push(s);
-            }
+            if (!stk.empty() && stk.top() == s) 
+                stk.pop();
+            else stk.push(s);
         }
 
         if (stk.empty()) answer++;
