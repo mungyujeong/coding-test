b1a0903d389fbfa62ef1f66cd2498e572fd3e7d2
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2023-12-26 21:40:18 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -1,83 +1,36 @@
 #include <iostream>
 #include <deque>
-#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
 int n, m, answer;
 deque<int> DQ;
-unordered_map<int, int> map;
-
-void update() {
-    int size = DQ.size();
-
-    for (int i = 1; i <= size; i++) {
-        map[DQ.front()] = i;
-        DQ.push_back(DQ.front());
-        DQ.pop_front();
-    }
-}
-
-void left() {
-    int front = DQ.front();
-    DQ.push_back(front);
-    DQ.pop_front();
-    answer++;
-
-    update();
-}
-
-void right() {
-    DQ.push_front(DQ.back());
-    DQ.pop_back();
-    answer++;
-
-    update();
-}
-
-void remove() {
-    map[DQ.front()] = -1;
-    DQ.pop_front();
-    update();
-}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> n >> m;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) 
         DQ.push_back(i);
-        map[i] = i;
-    }
 
     for (int i = 0; i < m; i++) {
         int x;
         cin >> x;
 
-        // cout << "map[t]\n";
-        // for (int t = 1; t <= 10; t++)
-        //     cout << t << ": " << map[t] << '\n';
-        // cout << endl;
-        // cout << "front: " << DQ.front() << '\n';
-
-        if (x == DQ.front()) {
-            remove();
-            continue;
+        int idx = find(DQ.begin(), DQ.end(), x) - DQ.begin();
+        while (DQ.front() != x) {
+            if (idx < (int)DQ.size() - idx) {
+                DQ.push_back(DQ.front());
+                DQ.pop_front();
+            } else {
+                DQ.push_front(DQ.back());
+                DQ.pop_back();
+            }
+            answer++;
         }
-
-        int left_time = map[x] - 1;
-        int right_time = DQ.size() - map[x] + 1;
-
-        if (left_time > right_time) {
-            while(right_time--)
-                right();
-        } else {
-            while (left_time--)
-                left();
-        }
-
-        remove();
+        DQ.pop_front();
     }
 
     cout << answer;
