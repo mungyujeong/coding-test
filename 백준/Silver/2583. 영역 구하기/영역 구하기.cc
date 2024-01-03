7b01eaf3a593c756b8df64965a5e69a4816dabc3
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-03 16:41:32 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-int m, n, k, answer = 1;
+int m, n, k;
 int board[MAX_N][MAX_N];
 vector<int> v;
 queue<pair<int, int>> Q;
@@ -16,23 +16,23 @@ int dx[] = {1, -1, 0, 0};
 int dy[] = {0, 0, 1, -1};
 
 int bfs(int sx, int sy) {
-    int cnt = 1;
+    int cnt = 0;
     board[sx][sy] = 1;
     Q.push({sx, sy});
     while (!Q.empty()) {
         int x, y;
         tie(x, y) = Q.front(); Q.pop();
+        cnt++;
         for (int d = 0; d < 4; d++) {
             int nx = x + dx[d];
             int ny = y + dy[d];
             if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
-            if (board[nx][ny] > 0 || board[nx][ny] == -1) continue;
-            board[nx][ny] = cnt++ + 1;
-            answer = max(answer, cnt);
+            if (board[nx][ny] == 1) continue;
+            board[nx][ny] = 1;
             Q.push({nx, ny});
         }
     }
-    return answer;
+    return cnt;
 }
 
 int main() {
@@ -42,25 +42,17 @@ int main() {
         cin >> x1 >> y1 >> x2 >> y2;
 
         for (int j = y1; j < y2; j++) 
-            fill(board[j] + x1, board[j] + x2, -1);
+            fill(board[j] + x1, board[j] + x2, 1);
     }
 
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            if (board[i][j] == 0) {
-                bfs(i, j);
-                v.push_back(answer);
-                answer = 1;
+            if (!board[i][j]) {
+                v.push_back(bfs(i, j));
             }
         }
     }
 
-    // for (int i = 0; i < m; i++) {
-    //     for (int j = 0; j < n; j++) {
-    //         cout << board[i][j] << ' ';
-    //     }
-    //     cout << endl;
-    // }
     sort(v.begin(), v.end());
     cout << v.size() << '\n';
     for (auto i : v)
