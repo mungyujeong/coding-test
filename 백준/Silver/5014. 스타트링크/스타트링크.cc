ace9935767304b8512c7cda3647ee2dcbd7643b1
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-03 18:13:31 +0900

    [Silver I] Title: 스타트링크, Time: 12 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.cc
+++ 백준/Silver/5014. 스타트링크/스타트링크.cc
@@ -8,34 +8,27 @@ using namespace std;
 int f, s, g, u, d;
 queue<int> Q;
 int board[MAX_N + 1];
-int visited[MAX_N + 1];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cin >> f >> s >> g >> u >> d;
 
+    fill(board + 1, board + f + 1, -1);
+    
     Q.push(s);
-    visited[s] = 1;
+    board[s] = 0;
 
     while (!Q.empty()) {
         int x;
         x = Q.front(); Q.pop();
-        int ux = x + u;
-        int dx = x - d;
-        if (ux <= f && !visited[ux]) {
-            board[ux] = board[x] + 1;
-            Q.push(ux);
-            visited[ux] = 1;
-        }
-        if (dx >= 1 && !visited[dx]) {
-            board[dx] = board[x] + 1;
-            Q.push(dx);
-            visited[dx] = 1;
+        for (auto nx : {x + u, x - d}) {
+            if (nx < 1 || nx > f || board[nx] != -1) continue;
+            Q.push(nx);
+            board[nx] = board[x] + 1;
         }
     }
 
-    if (s == g) cout << 0;
-    else if (board[g] == 0) cout << "use the stairs";
+    if (board[g] == -1) cout << "use the stairs";
     else cout << board[g];
 }
