bb1c7ed3b7419c7c42d346a7361cd96f963956a5
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-07 13:27:50 +0900

    [Gold III] Title: 다리 만들기, Time: 16 ms, Memory: 2140 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.cc
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.cc
@@ -63,13 +63,6 @@ void bridge(int sx, int sy) {
             }
         }
     }
-    // cout << "==============\n";
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j < n; j++) 
-    //         cout << length[i][j] << ' ';
-    //     cout << endl;
-    // }
-    // cout << "=====answer=====" << answer <<'\n';
 }
 
 int main() {
