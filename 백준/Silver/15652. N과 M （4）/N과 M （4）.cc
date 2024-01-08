5b26b6e78f4ac9c48b002290f4680eedc67d098d
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-08 21:16:30 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#define MAX_N 8
+
+using namespace std;
+
+int n, m;
+// int is_used[MAX_N];
+int arr[MAX_N];
+
+void print_comb() {
+    for (int i = 0; i < m; i++)
+        cout << arr[i] << ' ';
+    cout << '\n';
+}
+
+void backtracking(int k, int start) {
+    if (k == m) {
+        print_comb();
+        return;
+    }
+
+    for (int i = start; i <= n; i++) {
+        arr[k] = i;
+        backtracking(k + 1, i);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    backtracking(0, 1);
+}
