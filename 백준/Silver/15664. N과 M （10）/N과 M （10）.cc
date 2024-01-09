6fb3cf10b1c7a4b1e3f14bfda0221e995b68216e
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-09 14:54:04 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -8,7 +8,6 @@ using namespace std;
 int n, m;
 int arr[MAX_N];
 int result[MAX_N];
-int is_used[MAX_N];
 
 void backtracking(int curr, int idx) {
     if (curr == m) {
@@ -21,16 +20,15 @@ void backtracking(int curr, int idx) {
     int last = 0;
     for (int i = idx; i < n; i++) {
         if (last == arr[i]) continue;
-        if (is_used[i]) continue;
         last = arr[i];
         result[curr] = arr[i];
-        is_used[i] = 1;
         backtracking(curr + 1, i + 1);
-        is_used[i] = 0;
     }
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     cin >> n >> m;
     for (int i = 0;i < n; i++)
         cin>> arr[i];
