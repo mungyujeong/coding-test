5a31d8e84da7b383fad6ea674f74a82fefa6dfee
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-09 12:30:41 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#define MAX_N 30
+
+using namespace std;
+
+int n, s, cnt;
+int arr[MAX_N + 1];
+
+void backtracking(int curr, int sum) {
+    if (curr == n) {
+        if (s == sum)
+            cnt++;
+        return;
+    }
+
+    backtracking(curr + 1, sum);
+    backtracking(curr + 1, sum + arr[curr]);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> s;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    backtracking(0, 0);
+    if (s == 0) cnt--;
+    cout << cnt;
+}
