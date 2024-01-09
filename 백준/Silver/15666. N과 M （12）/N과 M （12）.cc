c8fc11db98eec1182a369baae999137a55f007b2
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-09 15:02:44 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 8
+
+using namespace std;
+
+int n, m;
+int arr[MAX_N];
+int result[MAX_N];
+
+void backtracking(int curr, int idx) {
+    if (curr == m) {
+        for (int i = 0; i < m; i++) 
+            cout << result[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    int last = 0;
+    for (int i = idx; i < n; i++) {
+        if (last == arr[i]) continue;
+        last = arr[i];
+        result[curr] = arr[i];
+        backtracking(curr + 1, i);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    sort(arr, arr + n);
+
+    backtracking(0, 0);
+}
