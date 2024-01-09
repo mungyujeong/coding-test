ef037d78c83349612d4e52b85b8519c190dd64a0
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-09 17:07:20 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 88 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+
+#define MAX_N 8
+
+using namespace std;
+
+int n, answer;
+int s[MAX_N];
+int w[MAX_N];
+
+void backtracking(int curr) {
+    if (curr == n) {
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] <= 0) 
+                cnt++;
+            // cout << s[i] << ' ';
+        }
+        // cout << endl;
+        answer = max(answer, cnt);
+        return;
+    }
+
+    if (s[curr] <= 0)
+        backtracking(curr + 1);
+    else {
+        for (int i = 0; i < n; i++) {
+            if (i == curr) continue;
+            if (s[i] <= 0) {
+                backtracking(curr + 1);
+                continue;
+            }
+            s[curr] -= w[i];
+            s[i] -= w[curr];
+            backtracking(curr + 1);
+            s[curr] += w[i];
+            s[i] += w[curr];
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) 
+        cin >> s[i] >> w[i];
+    
+    backtracking(0);
+    cout << answer;
+}
