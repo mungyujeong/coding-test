d83023dcd75b79e9b41d290eecd6edd64e8935c7
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-14 17:25:50 +0900

    [Silver IV] Title: 국영수, Time: 148 ms, Memory: 11260 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.cc
+++ 백준/Silver/10825. 국영수/국영수.cc
@@ -10,33 +10,15 @@ using namespace std;
 int n;
 vector<tuple<int, int, int, string>> student;
 
-int cmp(tuple<int, int, int, string> a, tuple<int, int, int, string> b) {
-    int kor1, kor2, eng1, eng2, math1, math2;
-    string name1, name2;
-    tie(kor1, eng1, math1, name1) = a;
-    tie(kor2, eng2, math2, name2) = b;
-
-    if (kor1 != kor2)
-        return a > b;
-    if (eng1 != eng2)
-        return a < b;
-    if (math1 != math2)
-        return a > b;
-    if (name1 != name2)
-        return a < b;
-
-    return a > b;
-}
-
 int main() {
     cin >> n;
     string name;
     int kor, eng, math;
     for (int i = 0; i < n; i++) {
         cin >> name >> kor >> eng >> math;
-        student.push_back({kor, eng, math, name});
+        student.push_back({-kor, eng, -math, name});
     }
-    sort(student.begin(), student.end(), cmp);
+    sort(student.begin(), student.end());
     for (auto i : student) {
         tie (ignore, ignore, ignore, name) = i;
         cout << name << '\n';
