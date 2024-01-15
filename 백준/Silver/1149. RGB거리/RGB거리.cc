72b31befa7c032ce20769b6e41c2429faf157a1a
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-15 13:10:39 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <climits>
+
+#define MAX_N 1'000
+
+using namespace std;
+
+int n;
+int arr[MAX_N][3];
+int d[MAX_N][3];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) 
+        cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
+    
+    d[0][0] = arr[0][0];
+    d[0][1] = arr[0][1];
+    d[0][2] = arr[0][2];
+
+    for (int i = 1; i < n; i++) {
+        d[i][0] = min(d[i - 1][1], d[i - 1][2]) + arr[i][0];
+        d[i][1] = min(d[i - 1][0], d[i - 1][2]) + arr[i][1];
+        d[i][2] = min(d[i - 1][0], d[i - 1][1]) + arr[i][2];
+    }
+
+    int answer = INT_MAX;
+    for (int i = 0; i < 3; i++) 
+        answer = min(answer, d[n - 1][i]);       
+    
+    cout << answer;
+}
