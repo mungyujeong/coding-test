9d10d8847879f89f3ce6aab8df8a623e901e0422
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-01-24 17:21:26 +0900

    [Gold II] Title: 불켜기, Time: 8 ms, Memory: 2856 KB -BaekjoonHub

--- 백준/Gold/11967. 불켜기/불켜기.cc
+++ 백준/Gold/11967. 불켜기/불켜기.cc
@@ -1,46 +1,83 @@
-#include <cstdio>
-#include <queue>
-#include <algorithm>
+#include <iostream>
 #include <vector>
+#include <queue>
+#include <tuple>
+
+#define MAX_N 105
+
 using namespace std;
-int N,M,cnt,visit[102][102],dy[4]={-1,1,0,0},dx[4]={0,0,1,-1}; 
-vector<pair<int,int>> adj[101][101];
-int main(){
-    scanf("%d%d",&N,&M);
-    while(M--){
-        int y,x,b,a;
-        scanf("%d%d%d%d",&x,&y,&a,&b);
-        adj[y][x].push_back({b,a});
+
+int n, m;
+vector<pair<int, int>> v[MAX_N][MAX_N];
+int board[MAX_N][MAX_N];
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+
+bool is_movable(int x, int y) {
+    for (int d = 0; d < 4; d++) {
+        int nx = x + dx[d];
+        int ny = y + dy[d];
+        if (nx < 1 || ny < 1 || nx > n || ny > n) continue;
+        if (board[nx][ny] == 2) {
+            board[x][y] = 1;
+            return true;
+        }
     }
-    queue<pair<int,int>> q;
-    q.push({1,1});
-    visit[1][1]=2;
-    int cnt=1;
-    while(!q.empty()){
-        int qsz=q.size();
-        auto f = q.front(); q.pop();
-        int y = f.first, x=f.second;
-        for(auto p : adj[y][x]){
-            int ny = p.first, nx = p.second;
-            if(!visit[ny][nx]){
-                for(int i=0;i<4;i++){
-                    int ty=ny+dy[i],tx=nx+dx[i];
-                    if(visit[ty][tx]==2) {
-                        q.push({ty,tx});
+
+    return false;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int x, y, a, b;
+        cin >> x >> y >> a >> b;
+        v[x][y].push_back({a, b});
+    }
+
+    queue<pair<int, int>> Q;
+    int x = 1;
+    int y = 1;
+    board[x][y] = 2;
+    Q.push({x, y});
+
+    do {
+        tie(x, y) = Q.front(); Q.pop();
+
+        // 해당 방에 있는 스위치로 불켜기
+        for (auto p : v[x][y]) {
+            if (!board[p.first][p.second]) {
+                board[p.first][p.second] = 1;
+                // 불 킨 방까지 이동할 수 있으면 추가
+                for (int d = 0; d < 4; d++) {
+                    int nx = p.first + dx[d];
+                    int ny = p.second + dy[d];
+                    if (nx < 1 || ny < 1 || nx > n || ny > n) continue;
+                    if (board[nx][ny] == 2) {
+                        Q.push({nx, ny});
                         break;
-                    }
+                    } 
                 }
-                visit[ny][nx]=1;
+            }
+        }   
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx < 1 || ny < 1 || nx > n || ny > n) continue;
+            if (board[nx][ny] == 1) {
+                board[nx][ny] = 2;
+                Q.push({nx, ny});
+            }
+        }
+    } while (!Q.empty());
+
+    int cnt = 0;
+    for (int i = 1; i <= MAX_N; i++)
+        for (int j = 1; j <= MAX_N; j++)
+            if (board[i][j] > 0)
                 cnt++;
-            }
-        }
-        for(int i=0;i<4;i++){
-            int ny = y+dy[i], nx= x+dx[i];
-            if(visit[ny][nx]==1){
-                visit[ny][nx]=2;
-                q.push({ny,nx});
-            }
-        }
-    }
-    printf("%d",cnt);
+    cout << cnt;
 }
