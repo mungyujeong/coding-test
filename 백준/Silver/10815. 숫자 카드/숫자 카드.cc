f9df178d1faf41bac4500931d54876760e40619a
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-02-01 17:18:31 +0900

    [Silver V] Title: 숫자 카드, Time: 224 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,24 +1,25 @@
 #include <iostream>
-#include <unordered_map>
+#include <algorithm>
+
+#define MAX_N 500'000
 
 using namespace std;
 using ll = long long;
 
-ll n, m, x;
-unordered_map<ll, bool> map;
+ll n, m, x, arr[MAX_N];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> x;
-        map[x] = true;
-    }
+    for (int i = 0; i < n; i++) 
+        cin >> arr[i];
+
+    sort(arr, arr + n);
+    
     cin >> m;
     for (int i = 0; i < m; i++) {
         cin >> x;
-        if (map[x]) cout << 1 << ' ';
-        else cout << 0 << ' ';
+        cout << binary_search(arr, arr + n, x) << ' ';
     }
 }
