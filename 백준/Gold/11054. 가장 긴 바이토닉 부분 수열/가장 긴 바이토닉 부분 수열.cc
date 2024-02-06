31a053d3b12a01d43fda2378282016e2ec5d3f4a
Moon <111988634+MoonGyu-Jeong@users.noreply.github.com>
2024-02-06 16:41:31 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -7,59 +7,6 @@ using namespace std;
 
 int n, arr[MAX_N], answer;
 
-int get_lis_reverse(int st, int en) {
-    int LIS[MAX_N] = {};
-    LIS[0] = arr[en - 1];
-    int len = 1;
-
-    for (int i = en - 1; i >= st; i--) {
-        if (arr[i] > LIS[len - 1]) {
-            LIS[len++] = arr[i];
-            continue;
-        }    
-        int idx = lower_bound(LIS, LIS + len, arr[i]) - LIS;
-        LIS[idx] = arr[i];
-    }
-
-    return len;
-}
-
-int get_lis(int st, int en) {
-    int result = 0;
-    int LIS[MAX_N] = {};
-    LIS[0] = arr[st];
-    int len = 1;
-
-    for (int i = st; i <= en; i++) {
-        if (arr[i] > LIS[len - 1]) {
-            LIS[len++] = arr[i];
-            continue;
-        }    
-        int idx = lower_bound(LIS, LIS + len, arr[i]) - LIS;
-        LIS[idx] = arr[i];
-    }
-
-    result += len;
-    int tmp = LIS[len - 1];
-
-    // 반대 방향
-    int R_LIS[MAX_N] = {};
-    R_LIS[0] = arr[n - 1];
-    len = 1;
-
-    for (int i = n - 1; i >= en; i--) {
-        if (arr[i] > R_LIS[len - 1]) {
-            R_LIS[len++] = arr[i];
-            continue;
-        }    
-        int idx = lower_bound(R_LIS, R_LIS + len, arr[i]) - R_LIS;
-        R_LIS[idx] = arr[i];
-    }
-    result += len;
-
-    return result - 1;
-}
-
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -67,11 +14,53 @@ int main() {
     for (int i = 0; i < n; i++)
         cin >> arr[i];
     
+    int LIS[MAX_N] = {};
+    int dist[MAX_N];
+    fill(dist, dist + n, 1);
+    LIS[0] = arr[0];
+    int len = 1;
+    int d = 1;
+
     for (int i = 0; i < n; i++) {
-        int result = 0;
-        result += get_lis(0, i);
-        answer = max(answer, result);
+        if (arr[i] > LIS[len - 1]) {
+            dist[i] = ++d;
+            LIS[len++] = arr[i];
+            continue;
+        }
+        else if (arr[i] == LIS[len - 1])
+            dist[i] = d;
+        int idx = lower_bound(LIS, LIS + len, arr[i]) - LIS;
+        LIS[idx] = arr[i];
     }
 
-    cout << answer;
+    int R_LIS[MAX_N] = {};
+    int r_dist[MAX_N] = {};
+    fill(r_dist, r_dist + n, 1);
+    R_LIS[0] = arr[n - 1];
+    len = 1;
+    d = 1;
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (arr[i] > R_LIS[len - 1]) {
+            r_dist[i] = ++d;
+            R_LIS[len++] = arr[i];
+            continue;
+        }
+        else if (arr[i] == R_LIS[len - 1])
+            r_dist[i] = d;
+        int idx = lower_bound(R_LIS, R_LIS + len, arr[i]) - R_LIS;
+        R_LIS[idx] = arr[i];
+    }
+
+    for (int i = 0; i < n; i++) 
+        answer = max(answer, dist[i] + r_dist[i]);
+
+    // for (int i = 0; i < n; i++)
+    //     cout << dist[i] << ' ';
+    // cout << endl;
+    // for (int i = 0 ; i< n;i++)
+    //     cout << r_dist[i] << ' ';
+    // cout << endl;
+
+    cout << answer - 1;
 }
