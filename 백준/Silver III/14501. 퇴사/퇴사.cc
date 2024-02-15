f2d27b533f36e5e03a55fad95a367cc403cd9f53
Mungyu Jeong <mungyujeong.dev@gmail.com>
2024-02-15 23:07:12 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver III/14501. 퇴사/퇴사.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#define MAX_N 20
+
+using namespace std;
+
+int n, dp[MAX_N], times[MAX_N], price[MAX_N];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 1; i <= n; i++) 
+        cin >> times[i] >> price[i];
+    
+    int max_val = 0;
+    for (int i = n; i >= 1; i--) {
+        int t = i + times[i];
+        if (t > n + 1) dp[i] = max_val;
+        else {
+            dp[i] = max(dp[t] + price[i], max_val);
+            max_val = dp[i];
+        }
+    }
+
+    cout << max_val;
+}
