1c30b41fbd6899311763356234f63e05c329f9be
Mungyu Jeong <mungyujeong.dev@gmail.com>
2024-02-16 23:10:24 +0900

    [Gold IV] Title: 수 묶기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.cc
+++ 백준/Gold/1744. 수 묶기/수 묶기.cc
@@ -1,67 +1,40 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 #define MAX_N 1'000
-#define NONE 1'001
 
 using namespace std;
 using ll = long long;
 
-ll n, arr[MAX_N], answer;
+ll n, x, answer;
+vector<ll> pl;
+vector<ll> mi;
+
+void solve(vector<ll> v) {
+    while (v.size() > 1) {
+        answer += *(v.end() - 1) * *(v.end() - 2);
+        v.pop_back();
+        v.pop_back();
+    }
+    if (!v.empty()) answer += v[0];
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cin >> n;
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    
-    sort(arr, arr + n, greater<ll>());
-    
-    ll a, b;
-    a = b = NONE;
-    int idx = 0;
-    // 1은 더하기
-    // 0은 매칭안되는 최소 마이너스랑 매칭
-    // 음수끼리, 양수끼리 큰 수끼리 매칭
     for (int i = 0; i < n; i++) {
-        if (arr[i] == 1) {
-            answer += 1;
-            continue;
-        }
-        if (arr[i] <= 0) {
-            idx = i;
-            break;
-        }
-
-        if (a == NONE) a = arr[i];
-        else if (b == NONE) b = arr[i];
-        else {
-            answer += (a * b);
-            a = arr[i];
-            b = NONE;
-        }
+        cin >> x;
+        if (x == 1) answer++;
+        else if (x > 0) pl.push_back(x);
+        else mi.push_back(x);
     }
+    sort(pl.begin(), pl.end());
+    sort(mi.begin(), mi.end(), greater<ll>());
 
-    if (a != NONE && b != NONE) answer += (a * b);
-    else if (a != NONE) answer += a;
-
-    a = b = NONE;
-    // 음수 매칭
-    for (int i = n - 1; i >= idx; i--) {
-        if (arr[i] > 0) break;
-        
-        if (a == NONE) a = arr[i];
-        else if (b == NONE) b = arr[i];
-        else {
-            answer += (a * b);
-            a = arr[i];
-            b = NONE;
-        }
-    }
-
-    if (a != NONE && b != NONE) answer += (a * b);
-    else if (a != NONE) answer += a;
+    solve(pl);
+    solve(mi);
 
     cout << answer;
 }
