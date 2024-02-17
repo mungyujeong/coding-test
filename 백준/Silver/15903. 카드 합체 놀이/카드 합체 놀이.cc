c36f1b9ab8acbb5c693f3f14d08809bd623832db
Mungyu Jeong <mungyujeong.dev@gmail.com>
2024-02-17 10:46:22 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
@@ -1,27 +1,36 @@
 #include <iostream>
 #include <algorithm>
+#include <queue>
 
 #define MAX_N 1'000
 
 using namespace std;
 using ll = long long;
 
-ll n, m, arr[MAX_N], answer;
+ll n, m, answer;
+priority_queue<ll> pq;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cin >> n >> m;
-    for (int i = 0; i < n; i++) 
-        cin >> arr[i];
-
-    while(m--) {
-        swap(arr[0], *min_element(arr, arr + n));
-        swap(arr[1], *min_element(arr + 1, arr + n));
-        arr[0] = arr[1] = arr[0] + arr[1];
+    for (int i = 0; i < n; i++) {
+        ll x;
+        cin >> x;
+        pq.push(-x);
     }
 
-    for (int i = 0; i < n; i++) 
-        answer += arr[i];
+    while(m--) {
+        ll a, b;
+        a = pq.top(); pq.pop();
+        b = pq.top(); pq.pop();
+        pq.push(a + b);
+        pq.push(a + b);
+    }
+
+    for (int i = 0; i < n; i++) {
+        answer += -pq.top();
+        pq.pop();
+    }
     cout << answer;
 }
