2359f1ee784bbfbb463512365125cb713ba4986a
Mungyu Jeong <mungyujeong.dev@gmail.com>
2024-02-17 10:40:21 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 716 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 1'000
+
+using namespace std;
+using ll = long long;
+
+ll n, m, arr[MAX_N], answer;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) 
+        cin >> arr[i];
+
+    while(m--) {
+        sort(arr, arr + n);
+        arr[0] = arr[1] = arr[0] + arr[1];
+    }
+
+    for (int i = 0; i < n; i++) 
+        answer += arr[i];
+    cout << answer;
+}
