53b66e14e7a5e230c329832e65dd22625134e099
Mungyu Jeong <mungyujeong.dev@gmail.com>
2024-02-17 10:42:00 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 24 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
@@ -16,7 +16,8 @@ int main() {
         cin >> arr[i];
 
     while(m--) {
-        sort(arr, arr + n);
+        swap(arr[0], *min_element(arr, arr + n));
+        swap(arr[1], *min_element(arr + 1, arr + n));
         arr[0] = arr[1] = arr[0] + arr[1];
     }
 
