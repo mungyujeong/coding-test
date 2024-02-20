2132cfa6c9cd653b75020ac2b3f6e9e6fd5e486b
Mungyu Jeong <mungyujeong.dev@gmail.com>
2024-02-20 23:04:42 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#define MAX_N 101
+
+using namespace std;
+
+int n, m, arr[MAX_N];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        arr[i] = i;
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        swap(arr[a], arr[b]);
+    }
+
+    for (int i = 1; i <= n; i++)
+        cout << arr[i] << ' ';
+}
