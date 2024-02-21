239af0abb129c4231cb5d95ea8e9035f7fc83aa3
Mungyu Jeong <mungyujeong.dev@gmail.com>
2024-02-21 23:58:01 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 60 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <climits>
+
+using namespace std;
+
+int test_case, n;
+
+int diff(string a, string b) {
+    int cnt = 0;
+    for (int i = 0; i < 4; i++)
+        if (a[i] != b[i]) 
+            cnt++;
+    return cnt;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> test_case;
+
+    while (test_case--) {
+        cin >> n;
+        vector<string> v;
+        unordered_map<string, int> map;
+        int answer = INT_MAX;
+
+        for (int i = 0; i < n; i++) {
+            string mbti;
+            cin >> mbti;
+            map[mbti]++;
+            if (map[mbti] <= 3) v.push_back(mbti);
+        }
+
+        int sz = v.size();
+        for (int i = 0; i < sz; i++) 
+            for (int j = i + 1; j < sz; j++) 
+                for (int k = j + 1; k < sz; k++) 
+                    answer = min(answer, diff(v[i], v[j]) + diff(v[i], v[k]) + diff(v[j], v[k]));
+
+        cout << answer << '\n';
+    }
+}
